05a5fba10607c5b6e3771ab5d048af3e2683047e
Kevin Chen <zephyrembyr@gmail.com>
2017-02-21 18:16:15 -0500

    v2.0 heapsort

--- Heapsort.java
+++ Heapsort.java
@@ -2,50 +2,64 @@ package Sorts;
 
 import java.time.*;
 
-public class Quicksort {
+class Heapsort {
 
     public static void main(String[] args)
     {
-        tester(1000);
-        tester(1000000);
-        tester(10000000);
-        tester(100000000);
-        tester(1000000000);
+        tester(10);
+        //tester(1000000);
+        //tester(10000000);
+        //tester(100000000);
+        //tester(1000000000);
     }
 
     private static double[] makeRandom(int n)
     {
-        double[] toBeSorted = new double[n];
-        for(int k = 0; k < n; k++)
-            toBeSorted[k] = Math.random() * n;
+        double[] toBeSorted = new double[n+1];
+        for(int k = 0; k < n+1; k++)
+            toBeSorted[k] = Math.random() * n+1;
         return toBeSorted;
     }
 
-    private static void sort(double[] A, int lBound, int rBound)
+    private static void sort(double[] A)
     {
-        int index;
-        if(lBound<rBound)
+        buildHeap(A);
+        removeMax(A, A.length);
+    }
+
+    private static void buildHeap(double[] A)
+    {
+        for(int k = 1; k < A.length; k++)
         {
-            index = partition(A,lBound,rBound);
-            sort(A, lBound, index-1);
-            sort(A, index+1, rBound);
+            int  index = k;
+            while(A[index] > A[index/2] && index>1)
+            {
+                swap(A, index, index/2);
+                index=index/2;
+            }
         }
     }
 
-    private static int partition(double[] A, int lBound, int rBound)
+    private static void heapify(double[] A, int k, int index)
     {
-        double pivot = A[rBound];
-        int i = lBound-1;
-        for(int k = lBound; k < rBound; k++)
+        int max = 2*k;
+        if(index > 0)
         {
-            if(A[k] <= pivot)
-            {
-                i++;
-                swap(A, i, k);
-            }
+            if(2*k > index)
+                removeMax(A, index);
+            if(2*k+1<=index)
+                max = Math.max(2*k,2*k+1);
+            swap(A, index, max);
+            heapify(A, max, index);
         }
-        swap(A, i+1, rBound);
-        return i+1;
+        
+    }
+
+    private static void removeMax(double[] A, int index)
+    {
+        swap(A, index, 1);
+        index--;
+        heapify(A, 1, index);
     }
 
     private static boolean isSorted(double[] A)
@@ -57,6 +71,7 @@ public class Quicksort {
         }
         return true;
     }
+
     private static void swap(double[] A, int p, int q)
     {
         double temp = A[p];
@@ -67,8 +82,11 @@ public class Quicksort {
     private static int tester(int n)
     {
         double[] test = makeRandom(n);
+        for(int k = 0; k < 10; k++){
+            System.out.println(test[k]);
+        }
         long startTime = System.currentTimeMillis();
-        sort(test,0, test.length-1);
+        sort(test);
         long endTime = System.currentTimeMillis();
         long timeElapsed = endTime-startTime;
         if(!isSorted(test))
@@ -77,6 +95,9 @@ public class Quicksort {
             return 1;
         }
         System.out.println("Sorting of "+n+" integers completed successfully in " +timeElapsed+ " milliseconds");
+        for(int k = 0; k < 10; k++){
+            System.out.println(test[k]);
+        }
         return 0;
     }
 }
